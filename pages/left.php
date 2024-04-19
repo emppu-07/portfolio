668356c110952c9e44db1380c5ae712f4175effb
emppu-07 <eemeli.sarttila@edu.tampere.fi>
2024-04-19 13:30:16 +0300

    switched back to html

--- pages/left.php
+++
@@ -1,9 +0,0 @@
-<div class="left">
-                <h2>Welcome to Empun kellari</h2> <hr>
-                <ul style="list-style-type: none;">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About Me</a></li>
-                    <li><a href="/stuff">My Stuff</a></li>
-                    <li><a href="/contact">Contacts</a></li>
-                </ul>
-            </div>
